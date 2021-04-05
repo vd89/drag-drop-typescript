c328b5ed4819166151bd5a45693acf3110c284d1
vd89 <dixitvora@gmail.com>
2021-04-05 18:07:42 +0530

    main_Chore: use of name space in typescript

---
+++ src/Components/projectItem.ts
@@ -0,0 +1,43 @@
+/// <reference path="baseComponent.ts" />
+
+namespace App {
+  // ProjectItem Class
+  export class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> implements Draggable {
+    private project: Project;
+
+    get persons() {
+      if (this.project.people === 1) {
+        return '1 person';
+      } else {
+        return `${this.project.people} persons`;
+      }
+    }
+
+    constructor(hostId: string, project: Project) {
+      super('single-project', hostId, false, project.id);
+      this.project = project;
+
+      this.configure();
+      this.renderContent();
+    }
+    @autobind
+    dragStartHandler(e: DragEvent): void {
+      e.dataTransfer!.setData('text/plain', this.project.id);
+      e.dataTransfer!.effectAllowed = 'move';
+    }
+    dragEndHandler(_: DragEvent): void {
+      console.log('DragEnd');
+    }
+
+    configure() {
+      this.element.addEventListener('dragstart', this.dragStartHandler);
+      this.element.addEventListener('dragend', this.dragEndHandler);
+    }
+
+    renderContent() {
+      this.element.querySelector('h2')!.textContent = this.project.title;
+      this.element.querySelector('h3')!.textContent = this.persons + ' assigned';
+      this.element.querySelector('p')!.textContent = this.project.description;
+    }
+  }
+}
