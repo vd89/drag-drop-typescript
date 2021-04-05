eaa0e843126ea88559e504175a321696ab5ba4c2
vd89 <dixitvora@gmail.com>
2021-04-05 17:44:39 +0530

    main_Chore: added the namespace for import files and code split

---
+++ src/dragDropInterfaces.ts
@@ -0,0 +1,12 @@
+namespace App {
+  // Drag and drop interfaces
+  export interface Draggable {
+    dragStartHandler(e: DragEvent): void;
+    dragEndHandler(e: DragEvent): void;
+  }
+  export interface DragTarget {
+    dragOverHandler(e: DragEvent): void;
+    dropHandler(e: DragEvent): void;
+    dragLeaveHandler(e: DragEvent): void;
+  }
+}
